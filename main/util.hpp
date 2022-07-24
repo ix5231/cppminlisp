ccef86c9a6ee5aad0204d0f6d0a64194fa9a4e38
Ix5231 <ix5231@gmail.com>
2022-07-24 11:49:40 +0900

    Add debug

--- main/util.hpp
+++ main/util.hpp
@@ -1,12 +1,17 @@
 #if !defined(UTIL_HPP)
 #define UTIL_HPP
 
+#include <cstdio>
 #include <stdexcept>
 
-class NotImplemented : public std::logic_error
-{
+#define debug(fmt, ...)                                                        \
+  fprintf(stderr, "[%s L%d] ", __FILE__, __LINE__);                            \
+  fprintf(stderr, fmt, __VA_ARGS__)
+#define debugln(msg) debug("%s\n", msg)
+
+class NotImplemented : public std::logic_error {
 public:
-    NotImplemented() : std::logic_error("Function not yet implemented") { };
+  NotImplemented() : std::logic_error("Function not yet implemented"){};
 };
 
 #endif // UTIL_HPP
